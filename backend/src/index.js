d27754f9ece9786ab3bbd4eec360790dc79d5d23
Brendo Maciel <maciel.brendoo@gmail.com>
2019-02-08 14:32:09 -0200

    First Commit

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send('Hello World');
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
+
+//Video dia 05.02 - tempo: 12:09
